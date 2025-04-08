1293f8b5c4c44cb2ae8f97871433a842ded7f493
Mohit Singh <mohitsinghjat7@gmail.com>
2025-04-09 00:18:54 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import connectDB from "./db/index.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
+connectDB()
+
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import express from "express";
+
+// const app = express()
+
+// (async () => {
+//     try{
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error", () => {
+//         console.log("ERROR: ",error);
+//         throw error
+//        })
+
+//        app.listen(process.env.PORT, () => {
+//         console.log(`App is listening on PORT : ${process.env.PORT}`);
+//        })
+//     }
+//     catch(error){
+//         console.error("ERROR : ", error)
+//         throw err
+//     }
+// })()
